a69d94cf4d643375f17eb3f5154c876785dbab1c
Jeffrey Lin <jeffrey@Jeffreys-MacBook-Air.local>
2021-11-29 00:30:29 -0800

    expression heatmap

--- project R-script .R
+++ project R-script .R
@@ -12,6 +12,8 @@ library(GO.db)
 library(GOstats)
 library(ggplot2)
 library("pheatmap")
+library(viridis)
+library(reshape2)
 
 #read meta data [file used: CLP = 85,87.htseq.out | CD8 = 17,20.htseq.out]
 metadata <- read_csv("metadata.csv")
@@ -70,7 +72,20 @@ significant_res <- subset(res1, padj < 0.05)
 up_in_CD8 <- subset(res1, padj < 0.05 & log2FoldChange > 0 )
 down_in_CD8 <- subset(res1, padj < 0.05 & log2FoldChange < 0 )
 
+#adding annotation to genes 
 
+res2 <- as.data.frame(results(dds, name = "cell_type_CD8_vs_CLP")) %>%
+  rownames_to_column("ENSEMBL")
+
+anno <- AnnotationDbi::select(org.Mm.eg.db,keys=res2$ENSEMBL,
+                              columns=c("ENSEMBL","SYMBOL","GENENAME","ENTREZID"),
+                              keytype="ENSEMBL") %>% filter(!duplicated(ENSEMBL))
+
+dim(res2)
+dim(anno)
+
+res <- left_join(res2, anno,by="ENSEMBL")
+head(res)
 ##result visulization 
 
 #logfold change - MA plot
@@ -100,21 +115,93 @@ select <- order(rowMeans(counts(dds,normalized =T )),
 df <- as.data.frame(colData(dds)[,c("cell_type")])
 pheatmap(assay(ntd)[select,], cluster_rows = F, show_rownames = F, cluster_cols = F, annotation_colors = df )
 
+#DE genes expression heatmap among samples
+ress <- results(dds, contrast = c("cell_type", "CD8", "CLP"))
+ress2 <- as.data.frame(ress)
 
-#adding annotation to genes 
+deseq2VST <- vst(dds)
+deseq2VST <- assay(deseq2VST)
+deseq2VST <- as.data.frame(deseq2VST)
+deseq2VST$Gene <- rownames(deseq2VST)
+head(deseq2VST)
 
-res2 <- as.data.frame(results(dds, name = "cell_type_CD8_vs_CLP")) %>%
-  rownames_to_column("ENSEMBL")
+sigGenes <- rownames(ress2[ress2$padj <= .05 & abs(ress2$log2FoldChange) > 3,])
 
-anno <- AnnotationDbi::select(org.Mm.eg.db,keys=res2$ENSEMBL,
-                              columns=c("ENSEMBL","SYMBOL","GENENAME","ENTREZID"),
-                              keytype="ENSEMBL") %>% filter(!duplicated(ENSEMBL))
+deseq2VST <- deseq2VST[deseq2VST$Gene %in% sigGenes,]
 
-dim(res2)
-dim(anno)
 
-res <- left_join(res2, anno,by="ENSEMBL")
-head(res)
+deseq2VST_wide <- deseq2VST
+deseq2VST_long <- melt(deseq2VST, id.vars=c("Gene"))
+
+head(deseq2VST_wide)
+head(deseq2VST_long)
+
+deseq2VST <- melt(deseq2VST, id.vars=c("Gene"))
+
+heatmap <- ggplot(deseq2VST, aes(x=variable, y=Gene, fill=value)) + geom_raster() + scale_fill_viridis(trans="sqrt") + theme(axis.text.x=element_text(angle=65, hjust=1), axis.text.y=element_blank(), axis.ticks.y=element_blank())
+heatmap
+
+##expression heatmap for top20 DE genes among samples (based on log2fold change)
+test <- res
+test <- rename(test, Gene = ENSEMBL)
+test <- rename(test, Gene_Name = SYMBOL)
+
+deseq2VST <- vst(dds)
+deseq2VST <- assay(deseq2VST)
+deseq2VST <- as.data.frame(deseq2VST)
+deseq2VST$Gene <- rownames(deseq2VST)
+head(deseq2VST)
+
+#upregulated in CD8
+ress_up <- as.data.frame(ress)  %>% 
+  arrange (, -log2FoldChange)
+
+ress_up <- subset(ress_up, padj < 0.05)
+
+sigGenes_up <- rownames(ress_up[1:23,])
+
+
+deseq2VST_up <- deseq2VST[deseq2VST$Gene %in% sigGenes_up,]
+
+deseq2VST_up_wide <- deseq2VST_up
+deseq2VST_up_long <- melt(deseq2VST_up, id.vars=c("Gene"))
+
+deseq2VST_up <- melt(deseq2VST_up, id.vars=c("Gene"))
+
+hm_up  <- merge(x = deseq2VST_up, y=test, by="Gene")
+
+heatmap_up <- ggplot(data = hm_up, mapping = aes(x = variable,
+                                                 y = Gene_Name,
+                                                 fill = value)) + geom_tile() + 
+  ggtitle("Top20 most Upregulated genes in CD8")+
+  scale_fill_gradient(low="light yellow", high="red")
+
+heatmap_up
+#downregulated in CD8
+ress_down <- as.data.frame(ress)  %>% 
+  arrange (, log2FoldChange)
+
+ress_down <- subset(ress_down , padj < 0.05)
+
+sigGenes_down <- rownames(ress_down[1:20,])
+
+deseq2VST_down <- deseq2VST[deseq2VST$Gene %in% sigGenes_down,]
+
+deseq2VST_wide_down <- deseq2VST_down
+deseq2VST_down_long <- melt(deseq2VST_down, id.vars=c("Gene"))
+
+deseq2VST_down <- melt(deseq2VST_down, id.vars=c("Gene"))
+
+hm_down  <- merge(x = deseq2VST_down , y=test, by="Gene")
+
+heatmap_down <- ggplot(data = hm_down, mapping = aes(x = variable,
+                                                     y = Gene_Name,
+                                                     fill = value)) + geom_tile() +
+  ggtitle("Top20 most Downregulated genes in CD8") +
+  scale_fill_gradient(low="light yellow", high="red")
+
+heatmap_down 
+
 
 #filtering annotated differentially expressed gene for export
 significant_res <- subset(res, padj < 0.05) %>% arrange (, padj)
